6bbee223d8c57b4b8b8ec445cef63bf1d6291ead
Muhammad Affan Khan <maffan@folio3pk.com>
2016-12-13 18:25:30 +0500

    initial commit

---
+++ greendao-generator/src/main/java/com/example/MainGenerator.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class MainGenerator {
+
+    public static void main(String[] args)  throws Exception {
+
+
+        Schema schema = new Schema(1,"com.app.maffan.mydaoapp.db");
+        Entity entity = schema.addEntity("User");
+        entity.addIdProperty();
+        entity.addStringProperty("first_name").notNull();
+        
+
+
+
+    }
+}
